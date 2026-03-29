660d0077b7c76a58f1a7cc40e1f65d2d0c449888
usamaru01 <sometinyworld@gmail.com>
2026-03-30 00:13:40 +0900

    アニメーション削除

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,12 @@
 import { initAccordion } from "./accordion.js";
 import { initFormValidation } from "./form.js";
 import { initModal } from "./modal.js";
-import { initScrollAnimation } from "./js.js";
 
 
 document.addEventListener('DOMContentLoaded',()=>{
 	initAccordion();
 	initFormValidation();
 	initModal();
-	initScrollAnimation();
 });
 
 
